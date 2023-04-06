35ee7e0e0c8f70cda507ed6abbeacc89c99988ea
Ivan <antufev-ivan@mail.ru>
2023-04-06 18:20:23 +0300

    maid changes in master

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log(Hello)
 
 console.log(branch develop)
+
+const userAge = 35;
+if (userAge >=18 ) {
+    console.log('Вы совершеннолетний');
+} else {
+    console.log('Вы не совершеннолетний');
+}
